99d52b9e5087b34e1bed0f57ddc2c8a7fb0b7631
Craig Ferry <PurityControl@users.noreply.github.com>
2015-11-22 11:10:08 +0000

    Complete etude 4-1.

---
+++ et-4-1/geom.ex
@@ -0,0 +1,26 @@
+defmodule Geom do
+  @moduledoc """
+  This is the geometry module required for the Elixir Etudes exercises.
+  Etude 4-1 uses pattern matching to distinguish between the shapes.
+  """
+  @vsn 0.1
+
+  @doc """
+  returns the area of a rectangle, ellipse or triangle providing the
+  arguments are positive integers.
+  If the shape or arguments aren't valid returns 0
+  """
+  @spec area({atom(), number(), number()}) :: number()
+
+  def area({shape, length, width}) when length > 0 and width > 0 do
+    case shape do
+      :rectangle -> width * length
+      :triangle ->  0.5 * width * length
+      :ellipse -> :math.pi * width * length
+      _ -> 0
+    end
+  end
+
+  def area({_, _, _}), do: 0
+end
+
