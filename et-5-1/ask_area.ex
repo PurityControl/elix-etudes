f78c49b2a72bb21dd0f4bef9c5642cbea4f5ce15
Craig Ferry <craig@puritycontrol.net>
2015-11-27 10:03:17 +0000

    first part of implementation get prompt to work without error checking.

---
+++ et-5-1/ask_area.ex
@@ -0,0 +1,53 @@
+defmodule AskArea do
+  def area do
+    shape = get_shape
+    width = get_width shape
+    height = get_height shape
+    Geom.area {shape, width, height}
+  end
+
+  defp get_shape do
+    IO.puts """
+      Which area do you want to calculate?
+      R)ectangle
+      T)riangle, or
+      E)llipse
+    """
+
+    answer = IO.gets("Which? > ")
+    value = String.first(answer)
+    char_to_area(value)
+  end
+
+  defp get_width shape do
+    prompt = case shape do
+      :rectangle -> "Enter width > "
+      :triangle -> "Enter base > "
+      :ellipse -> "Enter major radius > "
+    end
+    input = IO.gets prompt
+    value = String.strip input
+    {integer, _decimal} = Integer.parse value
+    integer
+  end
+
+  defp get_height shape do
+    prompt = case shape do
+      :rectangle -> "Enter height > "
+      :triangle -> "Enter height > "
+      :ellipse -> "Enter minor radius > "
+    end
+    input = IO.gets prompt
+    value = String.strip input
+    {integer, _decimal} = Integer.parse value
+    integer
+  end
+
+  defp char_to_area char do
+    case char do
+      "R" -> :rectangle
+      "T" -> :triangle
+      "E" -> :ellipse
+    end
+  end
+end
