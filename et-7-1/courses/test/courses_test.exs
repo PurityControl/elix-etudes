1e0f50debde581f3614199bbc485f2d3e9d25d98
Craig Ferry <craig@puritycontrol.net>
2015-12-13 12:12:39 +0000

    refactor testing hash

--- et-7-1/courses/test/courses_test.exs
+++ et-7-1/courses/test/courses_test.exs
@@ -3,9 +3,18 @@ defmodule CoursesTest do
   doctest Courses
 
   test "one string creates single item hash" do
-    dict = HashDict.new
-    dict = HashDict.put dict, "RF141", "ENGL 033"
     assert HashDict.equal?(
-      Courses.add(HashDict.new, "64850,ENGL 033,RF141"), dict)
+      Courses.add(HashDict.new, "64850,ENGL 033,RF141"), one_elem_hash)
+  end
+
+  test "adds if a key if not already present" do
+  end
+
+  test "appends if key is present" do
+  end
+
+
+  defp one_elem_hash do
+    dict = HashDict.put HashDict.new, "RF141", "ENGL 033"
   end
 end
