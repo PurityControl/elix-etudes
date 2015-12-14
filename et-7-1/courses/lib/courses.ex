d5a9110ba07e0c1c98a97038d8befa278da13630
Craig Ferry <craig@puritycontrol.net>
2015-12-14 07:42:07 +0000

    Pull data from a csv file.
    - reads whole file at once so have to be careful with large data
    - no error checking if file doesn't exist
    - no error checking f file is in wrong format

--- et-7-1/courses/lib/courses.ex
+++ et-7-1/courses/lib/courses.ex
@@ -1,18 +1,33 @@
 defmodule Courses do
+  def courses_from_csv file_path do
+    {_result, device} = File.open(file_path, [:read, :utf8])
+    file_text = IO.read(device, :all)
+    File.close device
+    file_text |> String.split "\n", trim: true 
+  end
+
   def add dict, str do
     HashDict.put dict,
       course_room(str),
       [course_name(str) | HashDict.get(dict, course_room(str), [])]
   end
 
+  def create_course_dict file_path do
+    courses = courses_from_csv file_path
+    create_course_dict courses, HashDict.new
+  end
+
+  defp create_course_dict [], dict do
+    dict
+  end
+
+  defp create_course_dict [course | rest], dict do
+    create_course_dict rest, add(dict, course)
+  end
+
   defp course_to_list str do
     str |> String.strip
         |> String.split ","
-  end
-
-  defp course_id str do
-    [id, _name, _room] = course_to_list str
-    id
   end
 
   defp course_name str do
